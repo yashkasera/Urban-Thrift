40da2cb8a1d1ca37b534dfbe8649fb1919b11796
deepg7 <deepgandhi151@gmail.com>
2022-11-09 01:38:51 +0530

    agenda implemented

--- backend/src/routes/watch.js
+++ backend/src/routes/watch.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const errorController = require("../controller/errorController");
 const authFunction = require("../middleware/authentication");
+const product_model = require("../model/Product");
 const user_product_model = require("../model/User_Product");
 const router = new Router();
 
@@ -10,14 +11,48 @@ router.post("/:product_id", async (req, res) => {
   try {
     const { product_id } = req.params;
     if (!product_id || !req.user._id) throw new Error();
+    const up = await user_product_model.findOne({
+      product_id,
+      user_id: req.user._id,
+    });
+    if (up) {
+      return res.status(203).send(up);
+    }
     const user_product = new user_product_model({
       product_id,
       user_id: req.user._id,
     });
     await user_product.save();
-    return res.status(200).send(user_product);
+    return res.status(201).send(user_product);
   } catch (e) {
     console.log(e);
     errorController(e, req, res);
   }
 });
+
+router.get("/", async (req, res) => {
+  try {
+    const watches = await user_product_model.find({ user_id: req.user._id });
+    const ids = watches.map((w) => w.product_id);
+    // console.log(ids);
+    const products = await product_model
+      .find({ _id: { $in: ids } })
+      .populate("added_by")
+      .populate("highest_bid_id");
+    // console.log(products);
+    return res.status(200).send(products);
+  } catch (e) {
+    console.log(e);
+    errorController(e, req, res);
+  }
+});
+
+router.post("/delete/:product_id", async (req, res) => {
+  try {
+    const watcher = await user_product_model.findOne({ user_id });
+  } catch (e) {
+    console.log(e);
+    errorController(e, req, res);
+  }
+});
+module.exports = router;
